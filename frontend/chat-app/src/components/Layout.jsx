51a68efb3307b38bb9138756ba971c67a770700a
Priyansh Kayastha <priyanshkayastha0803@gmail.com>
2025-07-14 00:27:30 +0530

    fix: correct Navbar import case

--- frontend/chat-app/src/components/Layout.jsx
+++ frontend/chat-app/src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Sidebar from "./Sidebar";
-import Navbar from "./Navbar";
+import Navbar from "./NavBar";
 
 const Layout = ({ children, showSidebar = false }) => {
   return (
